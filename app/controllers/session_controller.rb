79ee6b7a7afdbfb28447f9ab85e71ae64964dbcb
Jonathan See <mannysee@gmail.com>
2017-07-23 22:19:59 +1000

    Added Login Page - need to fix layout

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,25 @@
+class SessionController < ApplicationController
+
+  def create
+    user = User.find_by email: params[:email]
+    if user.present? and user.authenticate params[:password]
+      session[:user_id]=user.id
+      redirect_to new_post_path
+    else
+      flash[:error] = "Incorrect E-mail address and/or Password!"
+      redirect_to login_path
+    end
+  end
+
+  def new
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to login_path
+  end
+
+  def root
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  get '/app' => 'pages#app'
+
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  delete '/login' => 'session#destroy'
+
+  resource :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
